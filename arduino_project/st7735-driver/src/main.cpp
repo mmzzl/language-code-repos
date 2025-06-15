48dd9b675ff100f1318bbf6b8984ff9dd149e0d3
深空蓝 <369924201@qq.com>
2025-06-15 17:41:59 +0800

    [ADD] 轻度睡眠，节省电量

--- arduino_project/st7735-driver/src/main.cpp
+++ arduino_project/st7735-driver/src/main.cpp
@@ -65,6 +65,8 @@ static int32_t rnd_next(int32_t min, int32_t max);
 void processDate(weather_t *forecast);
 #define MAX_TOKENS 3
 #define DATE_LENGTH 16
+#define FULL_BRIGHTNESS 255
+#define LOW_BRIGHTNESS  30  // 降低亮度（0~255）
 
 // put function declarations here:
 // 更新标签文本的函数
@@ -236,6 +238,9 @@ static int32_t rnd_next(int32_t min, int32_t max)
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  ledcSetup(0, 5000, 8);
+  ledcAttachPin(TFT_BL, 0);
+  ledcWrite(0, FULL_BRIGHTNESS);
   // 初始化看门狗定时器
   String LVGL_Arduino = "Hello Arduino! ";
   LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() +
@@ -383,162 +388,6 @@ void setup() {
   if (timer == NULL) {
     printf("[%s:%d] lv_timer_create failed\n", __FUNCTION__, __LINE__);
   }
-
-
-
-  
-
-  // lv_obj_set_style_img_recolor_opa(img_bg, 0, LV_PART_MAIN|LV_STATE_DEFAULT);
-  // lv_obj_set_style_img_opa(img_bg, 255, LV_PART_MAIN|LV_STATE_DEFAULT);
-  // lv_obj_set_style_radius(img_bg, 0, LV_PART_MAIN|LV_STATE_DEFAULT);
-  // lv_obj_set_style_clip_corner(img_bg, true, LV_PART_MAIN|LV_STATE_DEFAULT);
-
-  
-  // static lv_style_t date_time_clock_style;
-  // lv_style_reset(&date_time_clock_style);
-  // lv_style_init(&date_time_clock_style);
-  // lv_style_set_radius(&date_time_clock_style, 5); //设置圆角样式
-  // lv_style_set_bg_opa(&date_time_clock_style,  LV_OPA_20); //设置背景透明度
-  // lv_style_set_border_width(&date_time_clock_style, 0); //设置边框宽度
-  // lv_style_set_bg_color(&date_time_clock_style, lv_color_black());
-  // lv_style_set_pad_left(&date_time_clock_style, 1); // 设置样式左边padding填充宽度
-  // lv_style_set_pad_right(&date_time_clock_style, 0); //设置样式右边padding填充宽度
-  // lv_style_set_pad_top(&date_time_clock_style, 0); //设置样式上边padding填充宽度
-  // lv_style_set_pad_bottom(&date_time_clock_style, 0); //设置样式下边padding填充宽度
-
-  // static lv_style_t time_style;
-  // lv_style_reset(&time_style);
-  // lv_style_init(&time_style);
-  // lv_style_set_bg_opa(&time_style, LV_OPA_COVER);
-  // lv_style_set_border_width(&time_style, 0);
-  // lv_style_set_radius(&time_style, 5);
-  // lv_style_set_bg_color(&time_style, lv_palette_darken(LV_PALETTE_BLUE, 3));
-  // lv_style_set_pad_left(&time_style, 0);
-  // lv_style_set_pad_right(&time_style, 0);
-  // lv_style_set_pad_top(&time_style, 0);
-  // lv_style_set_pad_bottom(&time_style, 0);
-
-  // static lv_style_t weather_style;
-  // lv_style_reset(&weather_style);
-  // lv_style_init(&weather_style);
-  // lv_style_set_bg_opa(&weather_style, LV_OPA_COVER);
-  // lv_style_set_border_width(&weather_style, 0);
-  // lv_style_set_radius(&weather_style, 5);
-  // lv_style_set_bg_color(&weather_style, lv_color_black());
-  // lv_style_set_pad_left(&weather_style, 0);
-  // lv_style_set_pad_right(&weather_style, 0);
-  // lv_style_set_pad_top(&weather_style, 0);
-  // lv_style_set_pad_bottom(&weather_style, 0);
-
-  // static lv_style_t date_style;
-  // lv_style_reset(&date_style);
-  // lv_style_init(&date_style);
-  // lv_style_set_bg_opa(&date_style, LV_OPA_COVER);
-  // lv_style_set_border_width(&date_style, 0);
-  // lv_style_set_radius(&date_style, 5);
-  // lv_style_set_bg_color(&date_style, lv_palette_darken(LV_PALETTE_BLUE, 3));
-  // lv_style_set_pad_left(&date_style, 0);
-  // lv_style_set_pad_right(&date_style, 0);
-
-  // static lv_style_t time_label_style; //实际标签样式
-  // lv_style_reset(&time_label_style);
-  // lv_style_init(&time_label_style);
-  // lv_style_set_text_color(&time_label_style, lv_color_white());
-  // lv_style_set_text_font(&time_label_style, &lv_font_montserrat_32);
-
-  // static lv_style_t date_label_style;
-  // lv_style_reset(&date_label_style);
-  // lv_style_init(&date_label_style);
-  // lv_style_set_text_opa(&date_label_style, LV_OPA_COVER);
-  // lv_style_set_bg_opa(&date_label_style, LV_OPA_0);
-  // lv_style_set_text_color(&date_label_style, lv_color_white());
-  // lv_style_set_text_font(&date_label_style, &lv_font_montserrat_18);
-
-  // static lv_style_t week_label_style;
-  // lv_style_reset(&week_label_style);
-  // lv_style_init(&week_label_style);
-  // // lv_style_set_text_opa(&week_label_style, LV_OPA_COVER);
-  // lv_style_set_bg_opa(&week_label_style, LV_OPA_0);
-  // lv_style_set_text_color(&week_label_style, lv_palette_main(LV_PALETTE_YELLOW));
-  // lv_style_set_text_font(&week_label_style, &zh_18);  
-  
-
-  // lv_obj_t *time_date_obj = lv_obj_create(lv_scr_act()); // 基于屏幕创建时间日期对象
-  // if (time_date_obj == NULL)
-  //   {
-  //       printf("[%s:%d] time_date_obj create failed\n", __FUNCTION__, __LINE__);
-  //       return;
-  //   }
- 
-	// lv_obj_set_size(time_date_obj, 320, 100); // 设置对象大小
-  // lv_obj_center(time_date_obj); // 对象居屏幕中间显示
-	// lv_obj_add_style(time_date_obj, &date_time_clock_style, LV_STATE_DEFAULT); //给time_date_obj对象添加样式
- 
-  // /*Time display*/
-  // lv_obj_t *time_obj = lv_obj_create(time_date_obj); // 基于time_date_obj对象创建时间对象
-  // if (time_obj == NULL)
-  // {
-  //     printf("[%s:%d] time_obj create failed\n", __FUNCTION__, __LINE__);
-  //     return;
-  // }
- 
-  // lv_obj_set_size(time_obj, 158, 100); // 设置对象大小
-  // lv_obj_align_to(time_obj, time_date_obj, LV_ALIGN_LEFT_MID, 0, 0); // 设置time_obj对象基于time_date_obj对象左边中间对齐
-  // lv_obj_add_style(time_obj, &time_style, LV_STATE_DEFAULT);  // 给time_obj对象添加样式
-
-  
-  // lv_clock.time_label = lv_label_create(time_obj);
-  // lv_obj_add_style(lv_clock.time_label, &time_label_style, LV_STATE_DEFAULT);
-  // // 创建一个定时器，每 1 秒调用一次 update_time_label 函数
-  
-  
-  // lv_clock.weekday_label = lv_label_create(time_obj);
-  // lv_obj_add_style(lv_clock.weekday_label, &week_label_style, LV_STATE_DEFAULT);
-
-  // lv_clock.date_label = lv_label_create(time_obj);
-  // lv_obj_add_style(lv_clock.date_label, &week_label_style, LV_STATE_DEFAULT);
-
-  // lv_clock.lunar_label = lv_label_create(time_obj);
-  // lv_obj_add_style(lv_clock.lunar_label, &week_label_style, LV_STATE_DEFAULT);
-
-  // lv_clock.lunar_term_label = lv_label_create(time_obj);
-  // lv_obj_add_style(lv_clock.lunar_term_label, &week_label_style, LV_STATE_DEFAULT);
-
-  //  // 设置时间标签lv_clock.time_label对象基于父对象居中对齐
-  // lv_obj_align_to(lv_clock.time_label, lv_obj_get_parent(lv_clock.time_label), LV_ALIGN_CENTER, 0, 0);
-  //  // 设置时间标签lv_clock.date_label对象基于父对象顶部中间对齐
-  // lv_obj_align_to(lv_clock.date_label, lv_obj_get_parent(lv_clock.date_label), LV_ALIGN_TOP_RIGHT, 2, 0);
-  // // 设置时间标签lv_clock.weekday_label对象基于父对象底部中间对齐
-  // lv_obj_align_to(lv_clock.weekday_label, lv_obj_get_parent(lv_clock.weekday_label),LV_ALIGN_TOP_LEFT, 0, 0);
-
-  // lv_obj_align_to(lv_clock.lunar_label, lv_obj_get_parent(lv_clock.lunar_label),LV_ALIGN_BOTTOM_LEFT, 0, 0);
-  // lv_obj_align_to(lv_clock.lunar_term_label, lv_obj_get_parent(lv_clock.lunar_term_label),LV_ALIGN_BOTTOM_RIGHT, 0, 0);
-
-  // // 显示天气信息
-  // lv_obj_t *weather_obj = lv_obj_create(time_date_obj); // 基于time_date_obj对象创建时间对象
-  // if (weather_obj == NULL)
-  // {
-  //     printf("[%s:%d] weather_obj create failed\n", __FUNCTION__, __LINE__);
-  //     return;
-  // }
- 
-  // lv_obj_set_size(weather_obj, 158, 100); // 设置对象大小
-  // lv_obj_align_to(weather_obj, time_date_obj, LV_ALIGN_RIGHT_MID, 0, 0); // 设置time_obj对象基于time_date_obj对象左边中间对齐
-  // lv_obj_add_style(weather_obj, &weather_style, LV_STATE_DEFAULT);  // 给time_obj对象添加样式
-
-  // lv_clock.weather_label = lv_label_create(weather_obj);
-  // lv_obj_add_style(lv_clock.weather_label, &week_label_style, LV_STATE_DEFAULT);
-  // lv_obj_align_to(lv_clock.weather_label, lv_obj_get_parent(lv_clock.weather_label),LV_ALIGN_TOP_LEFT, 0, 0);
-
-  // lv_clock.weather_icon = lv_img_create(weather_obj);
-
-  // lv_timer_t *timer = lv_timer_create(update_time_label, 200, (void *)&lv_clock);
-  // if (timer == NULL) {
-  //   printf("[%s:%d] lv_timer_create failed\n", __FUNCTION__, __LINE__);
-  // }
-
-  // std::string weather_str;
-  // showWeatherIcon(weather_str, weather_obj);
   Serial.println("Setup done");
   // 关闭NTP客户端
   timeClient.end();
@@ -546,11 +395,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   lv_timer_handler();
+  delay(50);
+  ledcWrite(0, LOW_BRIGHTNESS); // 降低亮度
+  esp_sleep_enable_timer_wakeup(5 * 1000000);
+  esp_light_sleep_start();
   if (!WiFi.isConnected()) {
     reconnectWiFi();
-  }
-  
-  delay(5);
+  } 
 }
 
 
